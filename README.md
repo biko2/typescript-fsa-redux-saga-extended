83df0e5d3b331d8fd6be43844efa4a9c4dc764e8
Alberto Silva <asilgag@gmail.com>
2018-02-12 13:58:53 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# [TypeScript FSA](https://github.com/aikoven/typescript-fsa) utilities for redux-saga [![npm version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url]
+ 
+## Installation
+
+```
+npm install --save typescript-fsa-redux-saga
+```
+
+## API
+
+### `bindAsyncAction(actionCreators: AsyncActionCreators): HigherOrderSaga`
+
+Creates higher-order-saga that wraps target saga with async actions.
+Resulting saga dispatches `started` action once started and `done`/`failed`
+upon finish.
+
+**Example:**
+
+```ts
+// actions.ts
+import actionCreatorFactory from 'typescript-fsa';
+
+const actionCreator = actionCreatorFactory();
+
+// specify parameters and result shapes as generic type arguments
+export const doSomething = 
+  actionCreator.async<{foo: string},   // parameter type
+                      {bar: number}    // result type
+                     >('DO_SOMETHING');
+                      
+// saga.ts
+import {SagaIterator} from 'redux-saga';
+import {call} from 'redux-saga/effects';
+import {doSomething} from './actions';
+                      
+const doSomethingWorker = bindAsyncAction(doSomething)(
+  function* (action): SagaIterator {
+    // `params` type is `{foo: string}`
+    const bar = yield call(fetchSomething, action.payload.foo);
+    return {bar};
+  },       
+);        
+           
+function* watchIncrementAsync() {
+    yield takeLatest(
+      doSomethingWorkerActionCreator.started,
+      doSomethingWorker,
+    );
+}
+```
+
+[npm-image]: https://badge.fury.io/js/typescript-fsa-redux-saga.svg
+[npm-url]: https://badge.fury.io/js/typescript-fsa-redux-saga
+[travis-image]: https://travis-ci.org/aikoven/typescript-fsa-redux-saga.svg?branch=master
+[travis-url]: https://travis-ci.org/aikoven/typescript-fsa-redux-saga
---
+++ src/index.ts
@@ -0,0 +1,53 @@
+import { isType, Action, AsyncActionCreators } from "typescript-fsa";
+import { SagaIterator } from "redux-saga";
+import { put, call, cancelled } from "redux-saga/effects";
+
+export function bindAsyncAction<P, R>(
+  actionCreators: AsyncActionCreators<P, R, any>,
+): {
+    (worker: (action: Action<typeof actionCreators.started>) => Promise<R> | SagaIterator):
+      (action: Action<typeof actionCreators.started>) => SagaIterator;
+
+    <A1>(worker: (action: Action<typeof actionCreators.started>, arg1: A1) => Promise<R> | SagaIterator):
+      (action: Action<typeof actionCreators.started>, arg1: A1) => SagaIterator;
+
+    <A1, A2>(worker: (action: Action<typeof actionCreators.started>, arg1: A1,
+      arg2: A2) => Promise<R> | SagaIterator):
+      (action: Action<typeof actionCreators.started>, arg1: A1, arg2: A2) => SagaIterator;
+
+    <A1, A2, A3>(worker: (action: Action<typeof actionCreators.started>, arg1: A1, arg2: A2, arg3: A3,
+      ...rest: any[]) => Promise<R> | SagaIterator):
+      (action: Action<typeof actionCreators.started>, arg1: A1, arg2: A2, arg3: A3, ...rest: any[]) => SagaIterator;
+  };
+
+export function bindAsyncAction<P, R>(
+  actionCreator: AsyncActionCreators<P, R, any>,
+) {
+  return (worker: (action: Action<typeof actionCreator.started>,
+    ...args: any[]) => Promise<any> | SagaIterator) => {
+    return function* (
+      action: Action<typeof actionCreator.started>,
+      ...args: any[],
+    ): SagaIterator {
+      if (isType(action, actionCreator.started)) {
+        try {
+          const result = yield (call as any)(worker, action, ...args);
+          yield put(actionCreator.done({
+            params: action.payload,
+            result,
+          }));
+        } catch (error) {
+          yield put(actionCreator.failed({ params: action.payload, error }));
+        } finally {
+          if (yield cancelled()) {
+            yield put(actionCreator.failed({
+              params: action.payload,
+              error: 'cancelled',
+            }));
+          }
+        }
+      }
+    };
+
+  };
+}
